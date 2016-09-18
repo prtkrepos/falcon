6dcb473cb4d776b918e4eeb00ed8ffec81eb9f7f
r3gz3n <akashmailbox722@gmail.com>
2016-09-18 05:52:56 +0530

    added submission model and problem page

--- problems/views.py
+++ problems/views.py
@@ -1,9 +1,44 @@
-from problems.models import Problem
-from problems.forms import ProblemForm
+from problems.models import Problems
+from submissions.models import Submissions
+from django.shortcuts import render
+
+
+def handle_uploaded_file(uploaded_file, language, sid):
+    """
+    Upload the file
+    """
+    filename = "~/falcon/allSubmissions/" + sid
+    if language == 'C':
+        filename += ".c"
+    elif language == 'C++':
+        filename += ".cpp"
+    with open(filename, "w") as f:
+        for line in uploaded_file:
+            f.write(line)
 
 
 def problem_page(request):
+    """
+    Problem Page View
+    """
     if request.method == 'POST':
+        submission = Submissions.objects.create()
+        sid = submission.id
+        language = request.POST['language']
+        handle_uploaded_file(request.FILES['solution'], language, sid)
+        submission.language = language
+        pid = request.GET['pid']
+        submission.pid = pid
+        template = 'game.html'
+        ctx = {}
+        ctx['submission'] = submission
+        return render(request, template, ctx)
     else:
-        problem = Problem.objects.create()
-
+        pid = request.GET['pid']
+        problem = Problems.objects.get(id=pid)
+        template = 'problems.html'
+        ctx = {}
+        ctx['problem_name'] = problem.problem_name
+        ctx['problem_statement'] = problem.problem_statement
+        ctx['starting_state'] = problem.starting_state
+        return render(request, template, ctx)
---
+++ submissions/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+CHOICES = (
+    ('C', 'c'),
+    ('C++', 'c++'),
+)
+
+class Submissions(models.model):
+    """
+    Stores the submission details
+    """
+    pid = models.IntegerField()
+    language = models.CharField(max_length='3', choices=CHOICES)
+    filepath = models.FilePathField(path='~/falcon/allSubmissions/')
